3519bea0160e04d5da0160c15b97919a5da5787f
nathanielpierson <nathanielepierson@gmail.com>
2025-01-18 14:42:44 -0500

    add controller and viewer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # forgery override here
   allow_browser versions: :modern
 end
---
+++ app/views/actors/_actor.json.jbuilder
@@ -0,0 +1,4 @@
+json.id actors.id
+json.first_name actors.first_name
+json.last_name actors.last_name
+json.known_for actors.known_for
