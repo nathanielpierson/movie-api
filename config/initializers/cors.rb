a77659ec7cd1967488b89a28f96e0990c526bc72
nathanielpierson <nathanielepierson@gmail.com>
2025-03-25 21:02:03 -0400

    update cors to support deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost:\d+/
+    origins /localhost:\d+/, "movies-frontend-1ofa.onrender.com/"
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
